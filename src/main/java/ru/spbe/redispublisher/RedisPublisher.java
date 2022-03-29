887d8de494aa0058692b34582a7905a7d8014c52
Dmitry Zinovev <zinovevdmv@gmail.com>
2022-03-29 17:52:19 +0300

    рефакторинг

--- src/main/java/ru/spbe/redispublisher/RedisPublisher.java
+++ src/main/java/ru/spbe/redispublisher/RedisPublisher.java
@@ -3,8 +3,8 @@ package ru.spbe.redispublisher;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import ru.spbe.redisstarter.IRedisSet;
 import ru.spbe.redisstarter.Redis;
+import ru.spbe.redisstarter.RedisSet;
 
 import java.util.HashSet;
 import java.util.List;
@@ -13,27 +13,27 @@ import java.util.Set;
 @RequiredArgsConstructor
 public class RedisPublisher<T> {
     private final Redis redis;
-    private final   IRedisSet set;
+    private final RedisSet set;
     private final ObjectMapper mapper;
 
-    public void addValue(T value) throws JsonProcessingException {
-        String valueString = mapper.writeValueAsString(value);
-        redis.addValue(set.getDataSetName(), valueString);
-        if((set.getChannelName() != null) && (!set.getChannelName().isEmpty())){
-            redis.publishMessage(set.getChannelName(), "addValue");
+    private void publishMessage() {
+        if ((set.getChannelName() != null) && (!set.getChannelName().isEmpty())) {
+            redis.publishMessage(set.getChannelName(), "PING");
         }
+    }
+
+    public void addValue(T value) throws JsonProcessingException {
+        redis.addValue(set.getDataSetName(), mapper.writeValueAsString(value));
+        publishMessage();
     }
 
     public void addValues(List<T> values) throws JsonProcessingException{
         Set<String> setValues = new HashSet<>();
         for(T t : values){
-            String valueString = mapper.writeValueAsString(t);
-            setValues.add(valueString);
+            setValues.add(mapper.writeValueAsString(t));
         }
         redis.addValues(set.getDataSetName(), setValues);
-        if((set.getChannelName() != null) && (!set.getChannelName().isEmpty())){
-            redis.publishMessage(set.getChannelName(), "addValues");
-        }
+        publishMessage();
     }
 
     public void clearValues(){
