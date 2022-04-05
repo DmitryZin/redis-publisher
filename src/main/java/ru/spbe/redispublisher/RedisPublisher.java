693e825a9cb0c1bed4654636f4c568db3f4b548c
Dmitry Zinovev <zinovevdmv@gmail.com>
2022-04-05 03:25:28 +0300

    Только key/value

--- src/main/java/ru/spbe/redispublisher/RedisPublisher.java
+++ src/main/java/ru/spbe/redispublisher/RedisPublisher.java
@@ -8,23 +8,8 @@ import ru.spbe.redisstarter.RedisSet;
 
 import java.util.*;
 
-/**
- * Публикация данных в Redis. Имеет возможность публиковать как просто справочник. так и key\value справочник
- * @param <T> Тип публикуемых данных
- *
- */
-
 @RequiredArgsConstructor
-public class RedisPublisher<T> {
-
-    /**
-     * хост сервера Redis
-     */
-    private final String host;
-    /**
-     * порт сервера Redis
-     */
-    private final int port;
+public class RedisPublisher<K, V> {
     /**
      * справочник
      */
@@ -37,8 +22,6 @@ public class RedisPublisher<T> {
     private final Redis redis;
 
     public RedisPublisher(String host, int port, RedisSet set, ObjectMapper mapper) {
-        this.host = host;
-        this.port = port;
         this.set = set;
         this.mapper = mapper;
 
@@ -51,31 +34,17 @@ public class RedisPublisher<T> {
         }
     }
 
-    public void publish(T value) throws JsonProcessingException {
-        redis.addValueToDataSet(set.getDataSetName(), mapper.writeValueAsString(value));
+    public void publish(K key, V value) throws JsonProcessingException {
+        redis.addValueToKeyDataSet(set.getDataSetName(), mapper.writeValueAsString(key), mapper.writeValueAsString(value));
         publishMessage();
     }
 
-    public void publish(String key, T value) throws JsonProcessingException {
-        redis.addValueToKeyDataSet(set.getDataSetName(), key, mapper.writeValueAsString(value));
-        publishMessage();
-    }
-
-    public void publish(Map<String, T> value) throws JsonProcessingException {
+    public void publish(Map<K, V> value) throws JsonProcessingException {
         Map<String, String> keys = new HashMap<>();
-        for (Map.Entry<String, T> map : value.entrySet()) {
-            keys.put(map.getKey(),  mapper.writeValueAsString(map.getValue()));
+        for (Map.Entry<K, V> map : value.entrySet()) {
+            keys.put(mapper.writeValueAsString(map.getKey()),  mapper.writeValueAsString(map.getValue()));
         }
         redis.addValuesToKeyDataSet(set.getDataSetName(), keys);
-        publishMessage();
-    }
-
-    public void publish(List<T> values) throws JsonProcessingException{
-        Set<String> setValues = new HashSet<>();
-        for(T t : values){
-            setValues.add(mapper.writeValueAsString(t));
-        }
-        redis.addValuesToDataSet(set.getDataSetName(), setValues);
         publishMessage();
     }
 
